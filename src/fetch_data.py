1ab14e412d8a5ac3a8acafbc433426b10d205144
luva-s <lucalaurintress@gmail.com>
2026-08-25 19:23:22 +0200

    added highest and lowest course

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-
 def fetch_data(days=7, coin="bitcoin", timegrain="hourly"):
 
     url = f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart"
@@ -15,11 +14,11 @@ def fetch_data(days=7, coin="bitcoin", timegrain="hourly"):
 
     }
 
-    #fetching the data
+    # fetching the data
     response = requests.get(url, params)
     data = response.json()
 
-    #loading into a data frame
+    # loading into a data frame
     df = pd.DataFrame(
         {"timestamp": [x[0] for x in data["prices"]],
          "price": [x[1] for x in data["prices"]],
@@ -28,24 +27,52 @@ def fetch_data(days=7, coin="bitcoin", timegrain="hourly"):
          }
     )
 
-    #formating data
+    # formating data
     df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
     df["price"] = round(df["price"], 2)
-    #formatting market caps and total volumes later 
+    # formatting market caps and total volumes later
 
     return df
 
+def create_charts(data):
+    fig, ax = plt.subplots(3)
+    fig.suptitle("Crypto market analysis dashboard")
+
+    #Overall course developement
+    ax[0].set_title("course developement")
+    x = data["timestamp"]
+    y = data["price"]
+    ax[0].plot(x, y)
+
+    #highest course 
+    ax[1].set_title("Highest course")
+    ax[1].axis("off")
+    max_course = max(data["price"])
+    ax[1].text(x=0.5,
+               y=0.6,
+               s= max_course,
+               fontsize=48,
+               fontweight='bold',
+               ha='center',
+               va='center',
+               color='#1f77b4',)
+
+    #lowest course
+    ax[2].set_title("Lowest course")
+    ax[2].axis("off")
+    min_course = min(data["price"])
+    ax[2].text(x=0.5,
+               y=0.6,
+               s= min_course,
+               fontsize=48,
+               fontweight='bold',
+               ha='center',
+               va='center',
+               color='#1f77b4',)
+    plt.show()
+
+
 
 if __name__ == "__main__":
-    data = fetch_data(coin="solana")
-
-    x = data["timestamp"]
-    y = data["price"] 
-
-    fig, ax = plt.subplots(2)
-    fig.suptitle("Crypto market analysis dashboard")
-    ax[0].plot(x, y)
-    ax[0].set_title("course developement")
-    ax[1].plot(x, -y)
-    ax[0].set_title("")
-    plt.show()
+    data = fetch_data(coin="bitcoin")
+    create_charts(data)
