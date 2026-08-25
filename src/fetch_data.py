79670b86b55c654ebc28b7aec7cab82ad1f5fc34
luva-s <lucalaurintress@gmail.com>
2026-08-25 10:29:19 +0200

    chart with correct values

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,18 +1,17 @@
 import requests
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 
 
-def fetch_data(days=7):
+def fetch_data(days=7, coin="bitcoin", timegrain="hourly"):
 
-    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+    url = f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart"
 
     params = {
         "vs_currency": "eur",
         "days": days,
-        "interval": "hourly"
+        "interval": timegrain
 
     }
 
@@ -38,11 +37,10 @@ def fetch_data(days=7):
 
 
 if __name__ == "__main__":
-    data = fetch_data()
-    print(data.head())
+    data = fetch_data(coin="solana")
 
-    x = np.linspace(0, 2 * np.pi, 200)
-    y = np.sin(x)
+    x = data["timestamp"]
+    y = data["price"] 
 
     fig, ax = plt.subplots()
     ax.plot(x, y)
